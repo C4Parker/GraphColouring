c25003d878a415bc402b4764a248c21eada130f5
Charlie Parker <censoredcharlie@gmail.com>
2018-10-31 14:41:05 +0000

    Refactored search and main method

--- SumColouring/JavaBacktracker/SumColour.java
+++ SumColouring/JavaBacktracker/SumColour.java
@@ -6,7 +6,7 @@ import java.util.*;
 *   the graph can be coloured with k colours.
 *   Command line arguments: <DIMACS file location> <k-colours>
 **/
-public class Backtracker {
+public class SumColour {
     // Initialise default values for graph
     static int size = -1;
     static boolean adjacency[][] = new boolean[1][1];
@@ -14,7 +14,6 @@ public class Backtracker {
     public static void main(String args[]) throws IOException{
         
         String inputFilename = args[0];
-        
         
         
         // Read in data from file
@@ -47,15 +46,36 @@ public class Backtracker {
         for(int i = 0; i < size; i++)
             for(int j = 0; j < size; j++)
                 if(adjacency[i][j])
-                    adjacency[j][i] = true;     
+                    adjacency[j][i] = true;   
         
-        Stack<Node> colouring = new Stack<Node>();
-        boolean chromaticNumerFound = false;
+        // Find upper bound colouring for graph
+        int maxOrder = 1;
+        for(int i = 0; i < size; i++){
+            int iOrder = 0;
+            for(int j = 0; j < size; j++)
+                if(adjacency[i][j])
+                    iOrder++;
+            if(iOrder > maxOrder)
+                maxOrder = iOrder;
+        }
+        
+        
+        
         
         long startTime = java.lang.System.currentTimeMillis();
         
+        // preliminary variables
+        boolean isColourable = true;
+        int bestSum = Integer.MAX_VALUE;
+        Stack<Node> colouring = new Stack<Node>();
+        Stack<Node> bestColouring = new Stack<Node>();
+        int k = maxOrder;
         
-        for(int k = 1; k < size && !chromaticNumerFound; k++){
+        //int targetSum = size * 4 ;
+        
+        
+        while(isColourable){//} && bestSum >= targetSum){
+            // Initialise domain of size k (maxOrder..1)
             ArrayList<Node> domain = new ArrayList<Node>(size);
             for(int i = 0; i < size; i++){
                 ArrayList<Integer> colours = new ArrayList<Integer>(k);
@@ -63,41 +83,64 @@ public class Backtracker {
                     colours.add(j+1);
                 Node n = new Node();
                 n.vertex = i;
+                int nOrder = 0;
+                for(int j = 0; j < size; j++)
+                    if(adjacency[i][j])
+                        nOrder++;
+                n.order = nOrder;
                 n.availableColours = colours;
                 n.colour = null;
+                
                 domain.add(n);
             }
             
-            chromaticNumerFound = search(domain, colouring);
-            if(chromaticNumerFound)
-                System.out.println("Chromatic number found: " + k);
+            isColourable = search(domain, colouring, bestSum, 0); // Use bestSum somehow so we can iterate and find better colourings rather than stopping once we find one colouring
+            int sum = 0;
+            for(Node n : colouring)
+                sum += n.colour;
+            if(sum <= bestSum){
+                bestSum = sum;
+                bestColouring.clear();
+                while(!colouring.isEmpty())
+                    bestColouring.push(colouring.pop());
+            }
+            colouring.clear(); // Reset stack
+            if(isColourable)
+                System.out.println(sum + " cost colouring found in " + (java.lang.System.currentTimeMillis()-startTime) + "ms");
         }
-        //System.out.println(search(domain, colouring));
-        
-        
-        long endTime = java.lang.System.currentTimeMillis();
-        System.out.println("Completed in " + Long.toString(endTime-startTime) + "ms");
-        
-        
         int sum = 0;
         for(Node n : colouring)
             sum += n.colour;
         System.out.println("Sum: "+sum);
         
+        long endTime = java.lang.System.currentTimeMillis();
+        System.out.println("Completed in " + Long.toString(endTime-startTime) + "ms");
+        
+        
+        
+        
         
     }
     
-    public static boolean search(ArrayList<Node> domain, Stack<Node> colouring){
+    public static boolean search(ArrayList<Node> domain, Stack<Node> colouring, int bestSum,int sum){
         if(domain.isEmpty())
-            return true;
+            return sum < bestSum;
         
-        //choose some node in domain
+        if(domain.size() + sum >= bestSum)
+            return false;
+        
+        // chooses vertex with minimum available colours
+        // tiebreaking on vertex cardinality
+        // first vertex found with only one available colour is coloured
         Node d = domain.get(0);
         int dVertex = d.vertex;
         int dColours = d.availableColours.size();
+        int maxOrder = 1;
         for(Node n : domain){
             int nColours = n.availableColours.size();
-            if (nColours < dColours){
+            if (nColours <= dColours){
+                if(nColours == dColours && n.order > maxOrder)
+                    maxOrder = n.order;
                 d = n;
                 dVertex = n.vertex;
                 dColours = n.availableColours.size();
@@ -120,9 +163,8 @@ public class Backtracker {
                         continue colourD;
                 }
             }
-            if(search(newDomain, colouring)){
+            if(search(newDomain, colouring, bestSum, sum+colour)){
                 d.colour = colour;
-                //String s = "(" + d.vertex + " : " + colour + ")";
                 colouring.push(d);
                 return true;
             }
@@ -133,13 +175,24 @@ public class Backtracker {
 
 class Node{
 	public int vertex;
+    public int order;
 	public ArrayList<Integer> availableColours;
     public Integer colour;
+    
+    /*public Node(int vertex, int order, ArrayList<Integer> availableColours, Integer colour){
+        this.vertex = vertex;
+        this.order = order;
+        this.availableColours = availableColours;
+        this.colour = colour;
+    }
+    public Node(){
+    }*/
     
     @Override
     public Node clone(){
         Node clone = new Node();
         clone.vertex = this.vertex;
+        clone.order = this.order;
         clone.availableColours = new ArrayList<Integer>();
         for(int c : this.availableColours)
             clone.availableColours.add(c);
