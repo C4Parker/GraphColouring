1e66a45c7d3fc1d28ec94f798834bbf13b694afb
Charlie Parker <censoredcharlie@gmail.com>
2019-02-19 12:51:34 +0000

    Text formatting bugfix

--- Colouring/GraphGenerator/SampleGenerator.java
+++ Colouring/GraphGenerator/SampleGenerator.java
@@ -2,9 +2,9 @@ public class SampleGenerator {
     
     public static void main(String args[]){
         for(int size = 10; size <= 40; size += 2)
-            for(double density = 0.05; density <= 0.95; density += 0.05)
+            for(double density = 0.05; density < 1; density += 0.05)
                 for(int i = 1; i <= 3; i++){
-                    String fname = "instances/"+size+"_"+density+"_"+i+".col";
+                    String fname = "instances/"+size+"_"+String.format("%.2f", density)+"_"+i+".col";
                     String fargs[] = {fname, Integer.toString(size), Double.toString(density)};
                     Generator.main(fargs);
                 }
